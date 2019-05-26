c37b5014a379e45a1a37ceaa082a4a6c014dcaa2
root <me@bryceyoder.com>
2019-05-26 17:55:24 -0400

    Add projects and improve CSS

--- home.js
+++ home.js
@@ -14,3 +14,8 @@ function letterAnim() {
 function appendLetter(el, letter, i) {
     window.setTimeout(function() {el.innerHTML = el.innerHTML + letter;}, 75*(i*2));
 }
+
+function goToURLHome(link) {
+    window.setTimeout(function(){ window.location=link;}, 500);
+    document.getElementById("home_container").style.setProperty('opacity', '0', 'important');
+}
